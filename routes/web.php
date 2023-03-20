126bb75824f3a44299fc33cb98b75d96d751985e
ellwo <ellw0pc@gmail.com>
2023-03-21 00:11:20 +0300

    first

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [AuthenticatedSessionController::class, 'create']);
+Route::get('/',[DashBoardController::class,'index'])
+->middleware(['auth', 'verified']);
 
 Route::get('/dashboard',[DashBoardController::class,'index'])
 ->middleware(['auth', 'verified'])->name('dashboard');
